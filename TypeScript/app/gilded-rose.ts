32830a0b9c0d78f17382c38164f9418171806d51
Rich Wareham <rich@richwareham.com>
2024-11-12 20:22:33 +0000

    feat: refactor updateItem

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -24,51 +24,46 @@ export class GildedRose {
   }
 
   updateItem(item: Item) {
-    if (
-      item.name != "Aged Brie" &&
-      item.name != "Backstage passes to a TAFKAL80ETC concert"
-    ) {
-      if (item.quality > 0) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-          item.quality = item.quality - 1;
-        }
+    // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    if (item.name === "Sulfuras, Hand of Ragnaros") {
+      return;
+    }
+
+    // deltas for sellIn and quality
+    let sellInDelta = -1, qualityDelta = -1;
+
+    // "Aged Brie" actually increases in Quality the older it gets
+    if (item.name === "Aged Brie") {
+      qualityDelta = 1;
+    }
+
+    // "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches
+    if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+      qualityDelta = 1;
+      // Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+      if (item.sellIn <= 10) {
+        qualityDelta = 2;
       }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
+      if (item.sellIn <= 5) {
+        qualityDelta = 3;
       }
     }
-    if (item.name != "Sulfuras, Hand of Ragnaros") {
-      item.sellIn = item.sellIn - 1;
-    }
-    if (item.sellIn < 0) {
-      if (item.name != "Aged Brie") {
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.quality > 0) {
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
+
+    // Once the sell by date has passed, Quality degrades twice as fast
+    if (item.sellIn <= 0) { qualityDelta *= 2; }
+
+    item.sellIn += sellInDelta;
+    item.quality += qualityDelta;
+
+    // The Quality of an item is never negative
+    if (item.quality < 0) { item.quality = 0; }
+
+    // The Quality of an item is never more than 50
+    if (item.quality >= 50) { item.quality = 50; }
+
+    // For backstage passes Quality drops to 0 after the concert
+    if ((item.name === "Backstage passes to a TAFKAL80ETC concert") && (item.sellIn < 0)) {
+      item.quality = 0;
     }
   }
 }
